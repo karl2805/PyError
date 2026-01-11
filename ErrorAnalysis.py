cf9affbe13fe9657053d062a69e08dd960644015
Karl Riedewald <karlriedewald@gmail.com>
2026-01-11 00:33:51 +0000

    error unit checking

--- ErrorAnalysis.py
+++ ErrorAnalysis.py
@@ -6,28 +6,47 @@ s.init_printing()
 
 pm = chr(177)
 
-class EQ:
+
+
+class EQ(object):
     def __init__(self, value:float, error:float, unit:u.Quantity, name:str=None, symbol:str='not_set'):
-        self.value = value * unit
-        self.error = error * unit
+        self._value = value * unit
+        self._error = error * unit
         self.unit = unit
         self.name = name
         self.symbol = sym.Symbol(symbol)
 
-    def __mul__(self:EQ, other:EQ):
-        left_value = self.value.to(u.meter)
-        left_error = self.error.to(u.meter)
+    def __mul__(self, other):
+        """
+        Multiples two EQ objects togeater
+        Uses the error propogation formula for error
+        """
+        if not isinstance(other, EQ):
+            return NotImplemented
+        
+        left_value = self._value
+        left_error = self._error
 
-        right_value = other.value.to(u.meter)
-        right_error = other.error.to(u.meter)
+        right_value = other._value
+        right_error = other._error
 
         result_value = left_value * right_value 
         result_error = result_value * ((left_error / left_value) + (right_error / right_value))
         
         return EQ(result_value.value, result_error, result_value.unit)
-
+    
+    def _check_units(self):
+        if not self._value.unit == self._error.unit == self.unit:
+            raise Exception("EQ object unit mismatch") 
+    
+    def si(self):
+        """
+        Convert the EQ to SI units
+        """
+        return EQ(self._value.si.value, self._error.si.value, self._value.si.unit, self.name)
+        
     def __str__(self):
-        return f"{self.name} = ({self.value.value} \xb1 {self.error.value}) {self.unit}"
+        return f"{self.name} = ({self._value.value} \xb1 {self._error.value}) {self.unit}"
         
 def measure_object(cls):
     
@@ -73,7 +92,8 @@ class Sun:
     pass
     
 sun = Sun(diameter = EQ(100, 50, u.m),
-          lum = EQ(10, 2, u.watt))
+          lum = EQ(5, 2, u.watt))
 
-
-print(sun)
+test = sun.diameter * sun.lum
+test.name = "test"
+print(test)
